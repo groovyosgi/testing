c2d2de313ac33c494f918f778aa777a7df9015aa
Lars Pfannenschmidt <lars@mobilecologne.de>
2013-07-26 15:56:39 +0200

    javadoc fix

--- com.github.groovyosgi.testing.pizzaservice.test/src/main/groovy/com/github/groovyosgi/testing/pizzaservice/impl/BundleContextProvider.groovy
+++ com.github.groovyosgi.testing.pizzaservice.test/src/main/groovy/com/github/groovyosgi/testing/pizzaservice/impl/BundleContextProvider.groovy
@@ -2,7 +2,7 @@ package com.github.groovyosgi.testing.pizzaservice.impl
 
 /**
  * This class provides the {@link BundleContext} via delegating the call to the
- * {@link Activator}. This is needed in order to keep the {@link Activator}
+ * {@link Activator}. This is needed in order to keep {@link Activator#getContext()}
  * package protected.
  * 
  */
